c3c90e8c5d5fafd2a3b34999549be504670cf047
aprillialee <aprilliapmlee@gmail.com>
2020-09-16 11:29:27 +0100

    added social media badges and styled them

--- components/HomeMenu/HomeMenu.jsx
+++ components/HomeMenu/HomeMenu.jsx
@@ -7,23 +7,39 @@ function HomeMenu() {
       <MenuItemStyled>ABOUT</MenuItemStyled>
       <MenuItemStyled>EPISODES</MenuItemStyled>
       <MenuItemStyled>CONTACT</MenuItemStyled>
+      <SocialMediaStyled>
+        <SocialMediaLogo src="/images/instagram.png" />
+        <SocialMediaLogo src="/images/twitter.png" />
+      </SocialMediaStyled>
     </HomeMenuStyled>
   );
 }
 
 const HomeMenuStyled = styled.div`
   width: 250px;
-  height: 230px;
+  height: 280px;
   position: fixed;
   bottom: 0px;
   right: 0px;
-  justify-content: center;
 `;
 
 const MenuItemStyled = styled.h1`
   font-size: ${rem("25px")};
   letter-spacing: 0.2rem;
   text-align: center;
+  margin-bottom: 20px;
+`;
+
+const SocialMediaStyled = styled.div`
+  width: 50%;
+  height: 25%;
+  margin: auto;
+`;
+
+const SocialMediaLogo = styled.img`
+  width: 45px;
+  height: 45px;
+  margin-left: 10px;
 `;
 
 export default HomeMenu;
