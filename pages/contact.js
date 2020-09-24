4a29c6cda4ae1d613641106a320595222b85ef1b
aprillialee <aprilliapmlee@gmail.com>
2020-09-24 15:33:32 +0100

    added some styling to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -21,7 +21,16 @@ function Contact() {
       {hasMounted && <Background />}
       <Header />
       <HomeMenu />
-      <ContactDiv />
+      <ContactDiv>
+        <ContactTitle>Transmit To The Funk Mothership</ContactTitle>
+        <FormDiv>
+          <form>
+            <Input type="text" placeholder="First Name" />
+
+            <Input type="text" placeholder="Second Name" />
+          </form>
+        </FormDiv>
+      </ContactDiv>
     </>
   );
 }
@@ -33,8 +42,33 @@ const ContactDiv = styled.div`
   width: 50%;
   height: 70%;
   display: flex;
-  background: #1e272c;
-  opacity: 0.6;
+  background: rgb(30, 39, 44, 0.6);
+  display: block;
 `;
 
+const ContactTitle = styled.h2`
+  font-size: 20px;
+  opacity: 1;
+  letter-spacing: 1px;
+  margin: 30px;
+`;
+
+const FormDiv = styled.div`
+  width: 100%;
+  height: 75%;
+  margin-top: 20px;
+  margin-left: 30px;
+  display: flex;
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  background: rgb(30, 39, 44);
+  border-radius: 3px;
+  width: 45%;
+  letter-spacing: 1px;
+  font-family: "Anton", sans-serif;
+  text-align: center;
+`;
 export default Contact;
